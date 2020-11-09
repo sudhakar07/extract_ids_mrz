6f594c217b9c9f0364d0740b6222eba4b7c8e88d
Sudhakar G <sudhakargk74@gmail.com>
2020-11-09 06:39:47 +0530

    comments

---
+++ app.py
@@ -0,0 +1,48 @@
+# EDA Pkgs
+import pandas as pd 
+import numpy as np 
+import time,json
+
+# Data Viz Pkg
+import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use("Agg")
+import seaborn as sns 
+import streamlit as st 
+import os
+
+
+from passporteye import read_mrz
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\sudhakar\AppData\Local\Tesseract-OCR\tesseract.exe'
+
+activities = ["Extract Ids","About"]	
+choice = st.sidebar.selectbox("Select Activities",activities)
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+if choice == 'Extract Ids':
+	st.subheader("Extract Ids")
+	
+	mrz = read_mrz("images/7.jpg")
+
+	if mrz is None:
+		print("Can not read image")
+
+
+	# Obtain image
+	mrz_data = mrz.to_dict()
+	st.write(mrz_data)
+	#st.write(mrz_data.get('sex'))
+
+
+elif choice == 'About':
+	st.subheader("About")
